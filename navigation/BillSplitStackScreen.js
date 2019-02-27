d8b634ac971e9cabd6af5af5812063dca7c61686
angela <ang_fl97@hotmail.com>
2019-02-27 15:25:15 -0800

    merge steven's friendlist

--- navigation/BillSplitStackScreen.js
+++ navigation/BillSplitStackScreen.js
@@ -3,12 +3,14 @@ import {createStackNavigator} from 'react-navigation';
 import BillSplit from '../screens/BillSplit';
 import SplitByItem from '../screens/SplitByItem';
 import SplitEvenly from '../screens/SplitEvenly';
+import FriendsList from '../screens/FriendsList';
 
 const BillSplitStack = createStackNavigator({
 
     BillSplit: { screen: BillSplit, navigationOptions: {header: null}},
     SplitByItem: { screen: SplitByItem},
     SplitEvenly: { screen: SplitEvenly},
+    FriendsList: {screen: FriendsList},
   },
   {
        initialRouteName: 'BillSplit',
--- screens/BillSplit.js
+++ screens/BillSplit.js
@@ -18,6 +18,14 @@ export default class BillSplit extends React.Component {
       <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
 
       <Text> Bill split screen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('SplitByItem')}>
+        <Text style={styles.btntext}>SPLIT BY ITEM</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={() => this.props.navigation.navigate('SplitEvenly')}>
+        <Text style={styles.btntext}>SPLIT EVENLY</Text>
+      </TouchableOpacity>
+
       </View>
 
       </View>
@@ -27,10 +35,38 @@ export default class BillSplit extends React.Component {
 
 
 const styles = StyleSheet.create({
-container:{
-  flex: 1,
-  // alignItems: 'center',
-  // justifyContent: 'center'
-}
-
+  container:{
+    flex: 1,
+    // alignItems: 'center',
+    // justifyContent: 'center'
+  },
+  header:{
+    fontSize:24,
+    color: "#000",
+    paddingBottom: 10,
+    marginBottom:40,
+    borderBottomColor: '#199187',
+    borderBottomWidth: 1,
+  },
+  textinput: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    height: 40,
+    marginBottom: 30,
+    color: "#000",
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#000',
+    width: '60%',
+    marginTop: 20,
+    marginBottom: 40,
+    alignSelf: 'center',
+  },
+  btntext:{
+    color: '#fff',
+    fontWeight: 'bold',
+  }
 });
