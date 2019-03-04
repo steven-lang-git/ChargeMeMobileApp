3c62434110040cdbeef00c0c7d0b0d07b087a4e1
Jasmin <jazzya0911@yahoo.com>
2019-03-04 09:56:41 -0800

    Updated Pages and Navigation for Settings Page

--- navigation/SettingsStackScreen.js
+++ navigation/SettingsStackScreen.js
@@ -7,17 +7,28 @@ import NotificationSettings from '../screens/NotificationSettings';
 import ChangePassword from '../screens/ChangePassword';
 import DeveloperGuide from '../screens/DeveloperGuide';
 import UserManual from '../screens/UserManual';
+import Bank from '../screens/Bank';
+import CreditCard from '../screens/CreditCard';
+import PushNotifications from '../screens/PushNotifications';
+import TextNotifications from '../screens/TextNotifications';
+import EmailNotifications from '../screens/EmailNotifications';
 import FAQs from '../screens/FAQs';
 
 const SettingsStack = createStackNavigator({
 
     SettingsScreen: { screen: SettingsScreen, navigationOptions: {header: null}},
     UserProfile: { screen: UserProfile},
+
     PaymentMethods: { screen: PaymentMethods},
+    Bank: {screen: Bank},
+    CreditCard: {screen: CreditCard},
+
     NotificationSettings: {screen: NotificationSettings},
+    PushNotifications: {screen: PushNotifications},
+    TextNotifications: {screen: TextNotifications},
+    EmailNotifications: {screen: EmailNotifications},
 
     ChangePassword: {screen: ChangePassword},
-
     DeveloperGuide: {screen: DeveloperGuide},
     UserManual: {screen: UserManual},
     FAQs: {screen: FAQs},
