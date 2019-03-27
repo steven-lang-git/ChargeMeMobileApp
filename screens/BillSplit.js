b5e164c80b4da9f9cad6282c72ddd52e407799f6
angela <ang_fl97@hotmail.com>
2019-03-26 20:15:03 -0700

    customized drawerNavigator

--- screens/BillSplit.js
+++ screens/BillSplit.js
@@ -10,7 +10,7 @@ export default class BillSplit extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <ImageBackground source={require('../assets/blue.jpg')} style={styles.imageContainer}>
+        <ImageBackground source={require('../assets/group-dinner.jpg')} style={styles.imageContainer}>
         <View style={styles.overlay} />
         <Header>
           <Left>
@@ -18,7 +18,11 @@ export default class BillSplit extends React.Component {
           </Left>
         </Header>
           <View style={styles.titleContainer}>
-            <Text style={styles.title}>Select Bill Split type</Text>
+            <Text style={styles.title}>Choose your Bill Split</Text>
+          </View>
+
+          <View style={styles.container}>
+            <Text/>
           </View>
 
           <View style={styles.infoContainer}>
@@ -45,9 +49,7 @@ export default class BillSplit extends React.Component {
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-    justifyContent: 'center',
-    alignContent: 'center',
-
+    alignItems: 'center',
   },
   errorMessage:{
     color: 'red',
@@ -57,10 +59,6 @@ const styles = StyleSheet.create({
   },
   signUpContainer: {
     flex:1,
-  },
-  datePickerContainer: {
-    flex:1,
-    backgroundColor: 'rgba(255,255,255,0.2)',
   },
   header:{
     position:'absolute',
@@ -73,62 +71,63 @@ const styles = StyleSheet.create({
       ...StyleSheet.absoluteFillObject,
       backgroundColor: 'rgba(69,85,117,0.7)',
   },
-logo: {
-  flex: 1,
-  resizeMode: 'contain',
-},
-titleContainer:{
-  justifyContent: 'center',
-  alignContent: 'center',
-  padding: 20,
-  flex: 1,
-  width: width,
-},
-infoContainer: {
-  flex: 2,
-  width: width,
-  padding:20,
-  // justifyContent: 'flex-start',
-  // alignContent: 'flex-start',
-},
-input: {
-  height:40,
-  backgroundColor: 'rgba(255,255,255,0.2)',
-  color:'#fff',
-  paddingHorizontal:10
-},
-title:{
-  fontWeight: 'bold',
-  color: '#fff',
-  fontSize: 25,
-  textAlign:'center',
-},
-inputTitle: {
-  color: 'white',
-  fontSize: 16,
-  fontWeight: 'bold',
-  marginBottom: 5,
-  marginTop: 10,
-},
+  logo: {
+    flex: 1,
+    resizeMode: 'contain',
+  },
+  titleContainer:{
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 20,
+    flex: 1,
+    width: width,
+  },
+  infoContainer: {
+    flex: 2,
+    width: width,
+    padding:20,
+  },
+  input: {
+    height:40,
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    color:'#fff',
+    marginBottom: 5,
+    paddingHorizontal:10,
+    borderWidth: 2,
+    borderRadius: 20,
+  },
+  title:{
+    fontWeight: 'bold',
+    color: '#fff',
+    fontSize: 25,
+    textAlign:'center',
+  },
+  inputTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    marginTop: 10,
+  },
 
-button: {
-  marginTop:10,
-  marginBottom: 10,
-  paddingTop:15,
-  paddingBottom:15,
-  marginLeft:30,
-  marginRight:30,
-  borderRadius:10,
-  borderWidth: 1,
-  borderColor: '#35b0d2',
-  backgroundColor: '#35b0d2',
+  button: {
+    marginTop:10,
+    marginBottom: 10,
+    paddingTop:15,
+    paddingBottom:15,
+    marginLeft:30,
+    marginRight:30,
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#35b0d2',
+    backgroundColor: '#35b0d2',
 
-},
-btntext:{
-  textAlign: 'center',
-  color: 'rgb(32,53,70)',
-  //fontWeight: 'bold',
-  color: 'white',
-  fontSize: 18,
-}
+  },
+  btntext:{
+    textAlign: 'center',
+    color: 'rgb(32,53,70)',
+    //fontWeight: 'bold',
+    color: 'white',
+    fontSize: 18,
+  }
 });
