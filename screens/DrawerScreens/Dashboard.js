75d349cdea4e55f304a6ba7cad3a11824f9fb83b
Jasmin <jazzya0911@yahoo.com>
2019-04-23 11:12:23 -0700

    fixed alignment of user information

--- screens/DrawerScreens/Dashboard.js
+++ screens/DrawerScreens/Dashboard.js
@@ -71,7 +71,7 @@ export default class Dashboard extends React.Component {
         .then ((snapshot) => {
           // for each user
           snapshot.forEach((childSnapShot) => {
-           
+
               tempArray.push({
                 key: childSnapShot.key,
                 first: childSnapShot.val().firstName,
@@ -83,7 +83,7 @@ export default class Dashboard extends React.Component {
               )
             });
             });
-    
+
         this.forceUpdate();
       })
 
@@ -114,8 +114,8 @@ export default class Dashboard extends React.Component {
       if(tempArray[x].key==item.charging){
       name=tempArray[x].first;
       }
-    };  
-    return <ListItem 
+    };
+    return <ListItem
     containerStyle= {styles.blueButton}
     title={item.name}
     titleStyle={{color:'white', fontWeight:'bold'}}
@@ -126,7 +126,7 @@ export default class Dashboard extends React.Component {
     rightTitleStyle={{color:'white'}}
     chevronColor="white"
     chevron
-  
+
     />;  }
   else if(item.charging==uid)
   {
@@ -135,8 +135,8 @@ export default class Dashboard extends React.Component {
       name=tempArray[x].first;
       }
     };
-    return <ListItem 
-    containerStyle= {styles.redButton}   
+    return <ListItem
+    containerStyle= {styles.redButton}
     title={item.name}
     titleStyle={{color:'white', fontWeight:'bold'}}
     subtitle={item.date }
@@ -146,7 +146,7 @@ export default class Dashboard extends React.Component {
     rightTitleStyle={{color:'white'}}
     chevronColor="white"
     chevron
-  
+
     />
   }
 };
@@ -155,7 +155,7 @@ export default class Dashboard extends React.Component {
 
 
 
-  renderItem = ({item})=> ( 
+  renderItem = ({item})=> (
     this.renderMain(item)
     )
   render() {
@@ -166,7 +166,7 @@ export default class Dashboard extends React.Component {
 
 
       <View style={styles.container}>
-        <KeyboardAwareScrollView keyboardShouldPersistTaps='always' extraScrollHeight={130}>
+
           <Header>
             <Left>
               <Icon name="bars" type="FontAwesome" onPress={()=>this.props.navigation.openDrawer()}/>
@@ -174,31 +174,36 @@ export default class Dashboard extends React.Component {
           </Header>
 
           <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+            <KeyboardAwareScrollView keyboardShouldPersistTaps='always' extraScrollHeight={130}>
 
-            <Avatar
-              size = "xlarge"
-              rounded title = {this.state.initials}
-            />
+            <View style={styles.userContainer}>
+              <Avatar
+                size = "xlarge"
+                rounded title = {this.state.initials}
+              />
 
-            <Text style={styles.text}>Welcome {this.state.firstName}</Text>
-            <Text style={styles.text}>{this.state.username}</Text>
+              <View style={styles.nameContainer}>
+                <Text style={styles.name}>Welcome {this.state.firstName}</Text>
+                <Text style={styles.username}>@{this.state.username}</Text>
+              </View>
+            </View>
 
 
 
 
-            <Text style={styles.text}>Your Recent Activity:</Text>
 
 
             <View style={styles.infoContainer}>
+              <Text style={styles.text}>Your Recent Activity:</Text>
               <FlatList style={{flex:1}}
                 keyExtractor={this.keyExtractor}
                 data={currentTransactions}
                 renderItem={this.renderItem}
               />
             </View>
-
+            </KeyboardAwareScrollView>
           </View>
-        </KeyboardAwareScrollView>
+
       </View>
       </ImageBackground>
     </SafeAreaView>
@@ -210,7 +215,11 @@ export default class Dashboard extends React.Component {
 const styles = StyleSheet.create({
 container:{
   flex: 1,
-  alignItems: "center",
+},
+nameContainer:{
+  flex: 1,
+  alignItems: 'center',
+  paddingTop: 15,
 },
 listItemContainer:{
   backgroundColor: '#fff',
@@ -221,16 +230,32 @@ listItem:{
   fontSize:20,
   padding: 10,
 },
+name:{
+  fontSize: 40,
+  color: "white",
+  paddingLeft: 20,
+  textAlign: 'center',
+},
+username:{
+  fontSize: 20,
+  color: "white",
+  paddingLeft: 20,
+  textAlign: 'center',
+},
 text:{
   fontSize: 25,
   color: "white",
+},
+userContainer:{
+  flex: 1,
+  flexDirection: 'row',
+  padding: 20,
 },
 infoContainer:{
   flex: 2,
   padding: 20,
   justifyContent: "flex-end",
   width:width,
-  marginTop:20,
 },
 blueButton:{
   padding:15,
@@ -249,7 +274,6 @@ redButton: {
   borderColor: 'coral',
   backgroundColor: 'coral',
   marginTop:10,
-  marginBottom: 10,
 },
 overlay:{
     ...StyleSheet.absoluteFillObject,
